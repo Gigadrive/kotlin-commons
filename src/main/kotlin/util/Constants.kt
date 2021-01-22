f8a0872ff46a4b80c2365c40c33316f6eb4a7e65
Mehdi Baaboura <mbaaboura@gigadrivegroup.com>
2021-01-22 05:13:01 +0100

    Added Gson

--- src/main/kotlin/util/Constants.kt
+++ src/main/kotlin/util/Constants.kt
@@ -1,10 +1,17 @@
 package com.gigadrivegroup.kotlincommons.util
 
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+
 public const val ALPHANUMERIC_CHARACTERS_UPPERCASE: String = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 public const val ALPHANUMERIC_CHARACTERS_LOWERCASE: String = "abcdefghijklmnopqrstuvwxyz"
 
 public const val ALPHANUMERIC_CHARACTERS_NUMBERS: String = "0123456789"
+
+public val GSON: Gson = Gson()
+
+public val GSON_PRETTY: Gson = GsonBuilder().setPrettyPrinting().create()
 
 public val ALPHANUMERIC_CHARACTERS: String =
     ALPHANUMERIC_CHARACTERS_UPPERCASE +
